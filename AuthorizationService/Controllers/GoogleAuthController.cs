b93da105146d953c72cbbb5cb9b5a3f4556a0cbf
Sander Wieringa <s.wieringa@student.fontys.nl>
2023-04-20 09:12:39 +0200

    added controllers

--- AuthorizationService/Controllers/GoogleAuthController.cs
+++ AuthorizationService/Controllers/GoogleAuthController.cs
@@ -1,17 +1,42 @@
 ﻿using Google.Apis.Auth;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using AuthorizationService.Data;
 
 namespace AuthorizationService.Controllers
 {
     public class GoogleAuthController : ControllerBase
     {
-        public GoogleAuthController()
-        {
+        private readonly IAuthService _authService;
 
+        public GoogleAuthController(IAuthService authService)
+        {
+            _authService = authService;
         }
 
-        [HttpPost]
+        [Route("signup")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Register()
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            var properties = new AuthenticationProperties { RedirectUri = Url.Action("GoogleResponse") };
+            return Challenge(properties, GoogleDefaults.AuthenticationScheme);
+        }
+
+        [Route("google-response")]
+        public async Task<IActionResult> GoogleResponse()
+        {
+            var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            var jwt = await _authService.AuthorizeAsync(result);
+
+            return Ok(jwt);
+        }
+
+        /*[HttpPost]
         [AllowAnonymous]
         [Route("test/{token}")]
         public async Task<IActionResult> Test([FromRoute] string token)
@@ -29,6 +54,6 @@ namespace AuthorizationService.Controllers
             {
                 return BadRequest();
             }
-        }
+        }*/
     }
 }
---
+++ AuthorizationService/Data/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace AuthorizationService.Data
+{
+    public interface IAuthService
+    {
+        Task<string> AuthorizeAsync(AuthenticateResult authResult);
+        Task<bool> CheckAccountExistsAsync(string email);
+    }
+}
