afaa7c4ff745bab45813dfe6f64456fb52750bba
Sander Wieringa <s.wieringa@student.fontys.nl>
2023-05-09 17:09:27 +0200

    publish test

--- AuthorizationService/Startup.cs
+++ AuthorizationService/Startup.cs
@@ -19,7 +19,7 @@ namespace AuthorizationService
             Configuration = configuration;
             _env = env;
         }
-
+        // publish test
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
